656f3b7db9228e9ad4aed98288d853b9a376982e
Alvis Logins <allogn@gmail.com>
2014-12-24 02:24:55 +0300

    strange run with dynamic SGH

---
+++ run/22.12/Axons/runTestAxons.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+rm -f ./SJ.csv
+
+for samplesize in {2000..10000..2000}
+do
+    for firstfile in {1..10..1}
+    do
+        for secondfile in {1..10..1}
+        do
+                for eps in {1..30..3}
+                do
+                    for alg in {4,6,7,8,9}
+                    do
+                        ../../../bin/SpatialJoin -a $alg -l 100 -e $eps -b 3 -t 1 -J 2 -i ../../../data/samplesDendrites/$samplesize/sample$firstfile.bin ../../../data/samplesAxons/$samplesize/sample$secondfile.bin
+                    done
+                done
+        done
+    done
+done
